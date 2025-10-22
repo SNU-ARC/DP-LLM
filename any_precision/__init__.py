08da359a5a247f490938de2aab4a75d358c551c2
kwonsw055 <kwonsw055@gmail.com>
2025-10-22 20:01:32 +0900

    Init DP-LLM

--- any_precision/__init__.py
+++ any_precision/__init__.py
@@ -1,3 +1,7 @@
 from . import modules
 from . import quantization
 from .modules import AnyPrecisionForCausalLM
+
+# DP-LLM
+from .modules import DPLLMForCausalLM
+from .modules import DPLLM_Finetune
--- any_precision/modules/__init__.py
+++ any_precision/modules/__init__.py
@@ -1,1 +1,5 @@
 from .AnyPrecisionForCausalLM import AnyPrecisionForCausalLM
+
+# DP-LLM
+from .DPLLM_Finetune import DPLLM_Finetune
+from .DPLLMForCausalLM import DPLLMForCausalLM
